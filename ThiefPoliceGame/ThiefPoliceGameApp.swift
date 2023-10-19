ae02548746444bf83aa3fc2b86e2315b3458b164
Lakshaya Sachdeva <lakshaya.sachdeva@colearn.id>
2023-10-19 11:45:13 +0530

    Initial Commit

---
+++ ThiefPoliceGame/ThiefPoliceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThiefPoliceGameApp.swift
+//  ThiefPoliceGame
+//
+//  Created by Lakshaya Sachdeva on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThiefPoliceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
